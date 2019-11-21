7114bd3ab1aa691e761284e82c2dd1d26971ed5b
AlanKostrick <alan@wecancodeit.org>
2019-11-20 21:14:57 -0600

    log messages

---
+++ js/components/Login.js
@@ -0,0 +1,9 @@
+export default function Login() {
+	return `
+    <div>
+        <input id="user-email" type="text" placeholder="Enter email" />
+        <input id="user-password" type="text" placeholder="Enter password" />
+        <button id="user-submit">Submit</button>
+    </div>
+    `;
+}
--- js/index.js
+++ js/index.js
@@ -1,5 +1,8 @@
 import Header from './components/Header';
 import Home from './components/Home';
+import Login from './components/Login';
+import Messages from './components/Messages';
+import firebase from './config/firebase';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.css';
 import '@fortawesome/fontawesome-free/css/all.css';
@@ -9,6 +12,8 @@ pageBuild();
 function pageBuild() {
 	renderHeader();
 	renderHome();
+	renderLogin();
+	renderMessages();
 }
 
 function renderHeader() {
@@ -21,5 +26,26 @@ function renderHome() {
 	home.addEventListener('click', () => {
 		const main = document.querySelector('.main');
 		main.innerHTML = Home();
+	});
+}
+
+function renderLogin() {
+	const loginButton = document.querySelector('.nav-list__login');
+	loginButton.addEventListener('click', () => {
+		const main = document.querySelector('.main');
+		main.innerHTML = Login();
+	});
+}
+
+function renderMessages() {
+	const messagesButton = document.querySelector('.nav-list__messages');
+	messagesButton.addEventListener('click', () => {
+		const main = document.querySelector('.main');
+		const db = firebase.firestore();
+		db.collection('messages')
+			.get()
+			.then(messages => {
+				main.innerHTML = Messages(messages);
+			});
 	});
 }
